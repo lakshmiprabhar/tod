9968a0314f7481da9c41f287d30f4bdad4c036a3
lakshmiprabha <lakshmiprabhar001@gmail.com>
2021-05-06 09:33:15 +0530

    helper is added

---
+++ app/helpers/format-date.js
@@ -0,0 +1,11 @@
+import moment from 'moment';
+
+
+
+import Ember from 'ember';
+
+export function formatDate(params){
+	return moment(params[0]).format('YYYY-MM-DD');
+}
+
+export default Ember.Helper.helper(formatDate);
